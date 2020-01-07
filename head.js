405c2cc449bedb5d7addebc50a6215d481f4670c
moe <mbchehade@gmail.com>
2020-01-07 01:38:49 +0000

    implement head

---
+++ head.js
@@ -0,0 +1,22 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+function head(item) {
+  if (item.length === 0) {
+    return undefined;
+  } else {
+    return item[0];
+  }
+}
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
