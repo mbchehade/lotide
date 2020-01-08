fd53c8b2a5fd187f51f3a9b038bc59b16e276a8b
moe <mbchehade@gmail.com>
2020-01-08 16:22:51 +0000

    array, removing unwanted elements

---
+++ without.js
@@ -0,0 +1,40 @@
+const assertEqualArray = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function without(arr1, arr2) {
+  let newArray = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr1.includes(arr2[i])) {
+      newArray.push(arr1[i]);
+
+
+    }
+  }
+  return newArray;
+}
+
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertEqualArray(words, ["hello", "world", "lighthouse"]);
+
+
