0d3e0abce4ad1c9deea455f82365f323c1370b8d
moe <mbchehade@gmail.com>
2020-01-13 18:25:44 +0000

    added module export

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+// function eqArrays(arr1, arr2) {
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+const eqArrays = require('./eqArray')
+const assertEqualArray = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+module.exports = assertEqualArray;
