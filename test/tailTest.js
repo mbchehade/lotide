0b787a88b624cb9676b3fd1f95e9b931e2de71ca
moe <mbchehade@gmail.com>
2020-01-13 21:36:14 +0000

    refactored it with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,13 +1,28 @@
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
+
+
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assertEqual = require('../assertEqual')
 
+describe("#tail", () => {
+  
+  it("return all items except the first one", () => {
+    
+    const words = ["hello", "lighthouse", "labs"];
+    assert.deepEqual(tail(words), ["lighthouse", "labs"]);
+  });
 
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+});
